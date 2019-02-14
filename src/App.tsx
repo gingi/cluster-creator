42e80df470f92214bf0e4e788a5ae5d3790956d1
Shiran Pasternak <shiran.pasternak@microsoft.com>
2019-02-14 14:05:55 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <DefaultButton>This is a button</DefaultButton>
+      </div>
+    );
+  }
+}
+
+export default App;
